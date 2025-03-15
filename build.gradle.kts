195a69612bda1f299b6d4a8f325a52dec1cfec2d
Asoji <asoji-git@hiitsdevin.mozmail.com>
2025-03-15 07:34:52 -0700

    ⬆️ update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,7 +45,7 @@ dependencies {
     }
     modLocalRuntime(libs.bundles.dev.mods)
 
-    include(modImplementation("gay.asoji:innerpastels:1.3.11+rev.ef2d5d6-branch.kt.1.21.main")!!)
+    include(modImplementation("gay.asoji:innerpastels:1.3.13+rev.e7bb3c8-branch.kt.1.21.main")!!)
     include(modImplementation("net.kyrptonaught:customportalapi:0.0.1-beta67-1.21")!!)
 }
 
