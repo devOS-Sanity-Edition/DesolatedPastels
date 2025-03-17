5c9c6e0cc8e4e0a755b80a939a6857971bf08f65
Naz <bluspringindohq@gmail.com>
2025-03-17 22:07:16 +0800

    Simplify EmissiveLeavesModel

--- src/main/kotlin/one/devos/nautical/desolatedpastels/client/model/EmissiveLeavesModel.kt
+++ src/main/kotlin/one/devos/nautical/desolatedpastels/client/model/EmissiveLeavesModel.kt
@@ -1,11 +1,7 @@
 package one.devos.nautical.desolatedpastels.client.model
 
-import net.fabricmc.fabric.api.renderer.v1.RendererAccess
-import net.fabricmc.fabric.api.renderer.v1.material.ShadeMode
 import net.fabricmc.fabric.api.renderer.v1.model.ForwardingBakedModel
-import net.fabricmc.fabric.api.renderer.v1.model.ModelHelper
 import net.fabricmc.fabric.api.renderer.v1.render.RenderContext
-import net.fabricmc.fabric.api.util.TriState
 import net.minecraft.client.resources.model.BakedModel
 import net.minecraft.core.BlockPos
 import net.minecraft.util.RandomSource
@@ -16,11 +12,6 @@ import java.util.function.Supplier
 class EmissiveLeavesModel(wrapped: BakedModel) : ForwardingBakedModel() {
     init {
         this.wrapped = wrapped
-    }
-
-    companion object {
-        const val LIGHT_VALUE = 0x000444
-        private val MATERIAL = RendererAccess.INSTANCE.renderer!!.materialFinder().shadeMode(ShadeMode.VANILLA).ambientOcclusion(TriState.TRUE).find()
     }
 
     override fun isVanillaAdapter(): Boolean {
@@ -34,21 +25,17 @@ class EmissiveLeavesModel(wrapped: BakedModel) : ForwardingBakedModel() {
         randomSupplier: Supplier<RandomSource>,
         context: RenderContext
     ) {
-        val emitter = context.emitter
-
-        for (i in 0 until ModelHelper.NULL_FACE_ID) {
-            val cullFace = ModelHelper.faceFromIndex(i)
-
-            if (!context.hasTransform() && context.isFaceCulled(cullFace))
-                continue
-
-            val quads = this.getQuads(state, cullFace, randomSupplier.get())
-            for (quad in quads) {
-                emitter.fromVanilla(quad, MATERIAL, cullFace)
-                emitter.lightmap(LIGHT_VALUE, LIGHT_VALUE, LIGHT_VALUE, LIGHT_VALUE)
-                emitter.emit()
-            }
+        context.pushTransform { quad ->
+            quad.lightmap(LIGHT_VALUE)
+            true
         }
+        super.emitBlockQuads(blockView, state, pos, randomSupplier, context)
+        context.popTransform()
     }
 
+    companion object {
+        // We're not actually sure how much light this really gives,
+        // but it looks good and works well enough.
+        const val LIGHT_VALUE = 0x000444
+    }
 }
