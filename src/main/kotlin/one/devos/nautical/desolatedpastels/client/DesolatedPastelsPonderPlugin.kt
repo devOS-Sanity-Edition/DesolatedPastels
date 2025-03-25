c402de92920fdac00ee3af5c06d72dcb1da0919b
Asoji <asoji-git@hiitsdevin.mozmail.com>
2025-03-25 01:38:36 -0700

    🚧 update ponder scenes, add portal scene

--- src/main/kotlin/one/devos/nautical/desolatedpastels/client/DesolatedPastelsPonderPlugin.kt
+++ src/main/kotlin/one/devos/nautical/desolatedpastels/client/DesolatedPastelsPonderPlugin.kt
@@ -5,6 +5,7 @@ import net.createmod.ponder.api.registration.PonderSceneRegistrationHelper
 import net.minecraft.core.registries.BuiltInRegistries
 import net.minecraft.resources.ResourceLocation
 import one.devos.nautical.desolatedpastels.DesolatedPastels
+import one.devos.nautical.desolatedpastels.client.ponder.scenes.PastelBlock
 import one.devos.nautical.desolatedpastels.client.ponder.scenes.PowderBlock
 import one.devos.nautical.softerpastels.common.SofterPastelsBlocks
 
@@ -15,9 +16,14 @@ class DesolatedPastelsPonderPlugin : PonderPlugin {
 
     override fun registerScenes(helper: PonderSceneRegistrationHelper<ResourceLocation>) {
         val powderBlock: ResourceLocation = BuiltInRegistries.BLOCK.getKey(SofterPastelsBlocks.WHITE_PASTEL_POWDER_BLOCK)
+        val pastelBlock: ResourceLocation = BuiltInRegistries.BLOCK.getKey(SofterPastelsBlocks.WHITE_PASTEL_BLOCK)
 
         helper.forComponents(powderBlock).addStoryBoard("blocks/powder_block") { scene, util ->
             PowderBlock.powderIntro(scene, util)
+        }
+
+        helper.forComponents(pastelBlock).addStoryBoard("blocks/portal") { scene, util ->
+            PastelBlock.portal(scene, util)
         }
     }
 }
