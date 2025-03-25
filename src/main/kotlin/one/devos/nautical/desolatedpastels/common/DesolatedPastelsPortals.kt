a5569c15d1bcd293c013ed6ae8a02de5d1c86096
Asoji <asoji-git@hiitsdevin.mozmail.com>
2025-03-25 01:26:58 -0700

    🔥 customportalsapi doesnt support multiple items

--- src/main/kotlin/one/devos/nautical/desolatedpastels/common/DesolatedPastelsPortals.kt
+++ src/main/kotlin/one/devos/nautical/desolatedpastels/common/DesolatedPastelsPortals.kt
@@ -10,7 +10,6 @@ object DesolatedPastelsPortals {
         CustomPortalBuilder.beginPortal()
             .frameBlock(SofterPastelsBlocks.WHITE_PASTEL_BLOCK)
             .lightWithItem(SofterPastelsItems.WHITE_POWDER)
-            .lightWithItem(DesolatedPastelsItems.PASTEL_SHARD)
             .destDimID(ResourceLocation.fromNamespaceAndPath("desolatedpastels", "desolated"))
             .tintColor(227,227,227)
             .registerPortal();
@@ -18,7 +17,6 @@ object DesolatedPastelsPortals {
         CustomPortalBuilder.beginPortal()
             .frameBlock(SofterPastelsBlocks.LIGHT_RED_PASTEL_BLOCK)
             .lightWithItem(SofterPastelsItems.LIGHT_RED_POWDER)
-            .lightWithItem(DesolatedPastelsItems.PASTEL_SHARD)
             .destDimID(ResourceLocation.fromNamespaceAndPath("desolatedpastels", "desolated"))
             .tintColor(197,136,136)
             .registerPortal();
@@ -26,7 +24,6 @@ object DesolatedPastelsPortals {
         CustomPortalBuilder.beginPortal()
             .frameBlock(SofterPastelsBlocks.RED_PASTEL_BLOCK)
             .lightWithItem(SofterPastelsItems.LIGHT_RED_POWDER)
-            .lightWithItem(DesolatedPastelsItems.PASTEL_SHARD)
             .destDimID(ResourceLocation.fromNamespaceAndPath("desolatedpastels", "desolated"))
             .tintColor(165,91,92)
             .registerPortal();
@@ -34,7 +31,6 @@ object DesolatedPastelsPortals {
         CustomPortalBuilder.beginPortal()
             .frameBlock(SofterPastelsBlocks.ORANGE_PASTEL_BLOCK)
             .lightWithItem(SofterPastelsItems.ORANGE_POWDER)
-            .lightWithItem(DesolatedPastelsItems.PASTEL_SHARD)
             .destDimID(ResourceLocation.fromNamespaceAndPath("desolatedpastels", "desolated"))
             .tintColor(186,117,95)
             .registerPortal();
@@ -42,7 +38,6 @@ object DesolatedPastelsPortals {
         CustomPortalBuilder.beginPortal()
             .frameBlock(SofterPastelsBlocks.YELLOW_PASTEL_BLOCK)
             .lightWithItem(SofterPastelsItems.YELLOW_POWDER)
-            .lightWithItem(DesolatedPastelsItems.PASTEL_SHARD)
             .destDimID(ResourceLocation.fromNamespaceAndPath("desolatedpastels", "desolated"))
             .tintColor(236,208,146)
             .registerPortal();
@@ -50,7 +45,6 @@ object DesolatedPastelsPortals {
         CustomPortalBuilder.beginPortal()
             .frameBlock(SofterPastelsBlocks.LIGHT_GREEN_PASTEL_BLOCK)
             .lightWithItem(SofterPastelsItems.LIGHT_GREEN_POWDER)
-            .lightWithItem(DesolatedPastelsItems.PASTEL_SHARD)
             .destDimID(ResourceLocation.fromNamespaceAndPath("desolatedpastels", "desolated"))
             .tintColor(135,187,125)
             .registerPortal();
@@ -58,7 +52,6 @@ object DesolatedPastelsPortals {
         CustomPortalBuilder.beginPortal()
             .frameBlock(SofterPastelsBlocks.GREEN_PASTEL_BLOCK)
             .lightWithItem(SofterPastelsItems.GREEN_POWDER)
-            .lightWithItem(DesolatedPastelsItems.PASTEL_SHARD)
             .destDimID(ResourceLocation.fromNamespaceAndPath("desolatedpastels", "desolated"))
             .tintColor(101,138,100)
             .registerPortal();
@@ -66,7 +59,6 @@ object DesolatedPastelsPortals {
         CustomPortalBuilder.beginPortal()
             .frameBlock(SofterPastelsBlocks.LIGHT_BLUE_PASTEL_BLOCK)
             .lightWithItem(SofterPastelsItems.LIGHT_BLUE_POWDER)
-            .lightWithItem(DesolatedPastelsItems.PASTEL_SHARD)
             .destDimID(ResourceLocation.fromNamespaceAndPath("desolatedpastels", "desolated"))
             .tintColor(114,171,188)
             .registerPortal();
@@ -74,7 +66,6 @@ object DesolatedPastelsPortals {
         CustomPortalBuilder.beginPortal()
             .frameBlock(SofterPastelsBlocks.BLUE_PASTEL_BLOCK)
             .lightWithItem(SofterPastelsItems.BLUE_POWDER)
-            .lightWithItem(DesolatedPastelsItems.PASTEL_SHARD)
             .destDimID(ResourceLocation.fromNamespaceAndPath("desolatedpastels", "desolated"))
             .tintColor(97,138,170)
             .registerPortal();
@@ -82,7 +73,6 @@ object DesolatedPastelsPortals {
         CustomPortalBuilder.beginPortal()
             .frameBlock(SofterPastelsBlocks.PURPLE_PASTEL_BLOCK)
             .lightWithItem(SofterPastelsItems.PURPLE_POWDER)
-            .lightWithItem(DesolatedPastelsItems.PASTEL_SHARD)
             .destDimID(ResourceLocation.fromNamespaceAndPath("desolatedpastels", "desolated"))
             .tintColor(130,111,197)
             .registerPortal();
@@ -90,7 +80,6 @@ object DesolatedPastelsPortals {
         CustomPortalBuilder.beginPortal()
             .frameBlock(SofterPastelsBlocks.MAGENTA_PASTEL_BLOCK)
             .lightWithItem(SofterPastelsItems.MAGENTA_POWDER)
-            .lightWithItem(DesolatedPastelsItems.PASTEL_SHARD)
             .destDimID(ResourceLocation.fromNamespaceAndPath("desolatedpastels", "desolated"))
             .tintColor(171,118,203)
             .registerPortal();
@@ -98,7 +87,6 @@ object DesolatedPastelsPortals {
         CustomPortalBuilder.beginPortal()
             .frameBlock(SofterPastelsBlocks.BROWN_PASTEL_BLOCK)
             .lightWithItem(SofterPastelsItems.BROWN_POWDER)
-            .lightWithItem(DesolatedPastelsItems.PASTEL_SHARD)
             .destDimID(ResourceLocation.fromNamespaceAndPath("desolatedpastels", "desolated"))
             .tintColor(120,89,73)
             .registerPortal();
@@ -106,7 +94,6 @@ object DesolatedPastelsPortals {
         CustomPortalBuilder.beginPortal()
             .frameBlock(SofterPastelsBlocks.LIGHT_GRAY_PASTEL_BLOCK)
             .lightWithItem(SofterPastelsItems.LIGHT_GRAY_POWDER)
-            .lightWithItem(DesolatedPastelsItems.PASTEL_SHARD)
             .destDimID(ResourceLocation.fromNamespaceAndPath("desolatedpastels", "desolated"))
             .tintColor(118,132,133)
             .registerPortal();
@@ -114,7 +101,6 @@ object DesolatedPastelsPortals {
         CustomPortalBuilder.beginPortal()
             .frameBlock(SofterPastelsBlocks.GRAY_PASTEL_BLOCK)
             .lightWithItem(SofterPastelsItems.GRAY_POWDER)
-            .lightWithItem(DesolatedPastelsItems.PASTEL_SHARD)
             .destDimID(ResourceLocation.fromNamespaceAndPath("desolatedpastels", "desolated"))
             .tintColor(76,91,91)
             .registerPortal();
@@ -122,7 +108,6 @@ object DesolatedPastelsPortals {
         CustomPortalBuilder.beginPortal()
             .frameBlock(SofterPastelsBlocks.BLACK_PASTEL_BLOCK)
             .lightWithItem(SofterPastelsItems.BLACK_POWDER)
-            .lightWithItem(DesolatedPastelsItems.PASTEL_SHARD)
             .destDimID(ResourceLocation.fromNamespaceAndPath("desolatedpastels", "desolated"))
             .tintColor(29,30,31)
             .registerPortal();
