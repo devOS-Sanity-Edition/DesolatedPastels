568f8e29ef85764cc008d1c47eeb6354b3537a5c
Asoji <asoji-git@hiitsdevin.mozmail.com>
2025-04-19 22:49:48 -0700

    🔨 update ponder

--- build.gradle.kts
+++ build.gradle.kts
@@ -61,11 +61,10 @@ dependencies {
 
     include(modImplementation("gay.asoji:innerpastels:1.3.17+rev.45403a4-branch.kt.1.21.main")!!)
     include(modImplementation("net.kyrptonaught:customportalapi:0.0.1-beta68-1.21")!!)
-//    include(modImplementation(files("libs/Ponder-Fabric-1.21.1-1.0.0.jar"))!!) // using max's fork of ponder for the time being https://github.com/maximumpower55/Ponder/tree/mc1.21.1/dev
-    include(modImplementation(":Ponder-Fabric:1.21.1-1.0.0")!!) // using max's fork of ponder for the time being https://github.com/maximumpower55/Ponder/tree/mc1.21.1/dev
     modRuntimeOnly(libs.fcapi)
     modApi(libs.flywheel.api)
     modImplementation(libs.flywheel)
+    include(modImplementation("net.createmod.ponder:Ponder-Fabric-1.21.1:1.0.48")!!)
 }
 
 // Write the version to the fabric.mod.json
